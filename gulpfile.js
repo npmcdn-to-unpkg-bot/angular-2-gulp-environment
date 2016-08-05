713fa604a9d86a5834b24646b76639eb3ec4ce11
Rajagopal R <Rajagopal.R@nationstarmail.com>
2016-08-05 13:20:23 +0530

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver'),
+    typescript = require('gulp-typescript'),
+    sourcemaps = require('gulp-sourcemaps'),
+    tscConfig = require('./tsconfig.json');
+
+var appSrc = 'builds/development/',
+    tsSrc = 'app/typescript/';
+
+gulp.task('html', function() {
+  gulp.src(appSrc + '**/*.html');
+});
+
+gulp.task('css', function() {
+  gulp.src(appSrc + '**/*.css');
+});
+
+//
+// gulp.task("libs", () => {
+//     return gulp.src([
+//             'es6-shim/es6-shim.min.js',
+//             'systemjs/dist/system-polyfills.js',
+//             'systemjs/dist/system.src.js',
+//             'reflect-metadata/Reflect.js',
+//             'rxjs/**',
+//             'zone.js/dist/**',
+//             '@angular/**'
+//         ], {cwd: "node_modules/**"}) /* Glob required here. */
+//         .pipe(gulp.dest("build/lib"));
+// });
+//
+gulp.task('copylibs', function() {
+  return gulp
+    .src([
+            'es6-shim/es6-shim.min.js',
+            'systemjs/dist/system-polyfills.js',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/Reflect.js',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**'
+        ], {cwd: "node_modules/**"})
+    .pipe(gulp.dest(appSrc + 'js/lib/angular2'));
+});
+
+gulp.task('typescript', function () {
+  return gulp
+    .src(tsSrc + '**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(appSrc + 'js/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(tsSrc + '**/*.ts', ['typescript']);
+  gulp.watch(appSrc + 'css/*.css', ['css']);
+  gulp.watch(appSrc + '**/*.html', ['html']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src(appSrc)
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['copylibs', 'typescript', 'watch', 'webserver']);
